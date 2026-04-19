bd59dae3d398b6f00aba1c4aef085d42772b9729
Amit Kumar Sahni <sahniamitkumar2017@gmail.com>
2026-04-19 22:42:46 +0530

    remove docs and made worker as 1

--- api/main.py
+++ api/main.py
@@ -11,12 +11,12 @@ from .models import Dog, Comment, Post, Image, User, SessionLocal
 
 load_dotenv()
 
-app = FastAPI(
-    docs_url=None,
-    redoc_url=None,
-    openapi_url="/openapi.json"
-)
-
+# app = FastAPI(
+#     docs_url=None,
+#     redoc_url=None,
+#     openapi_url="/openapi.json"
+# )
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[os.getenv("API_URL"), "http://", "http://*ap-south*"],  # The default React port
@@ -73,10 +73,10 @@ app.include_router(dogs.router)
 app.include_router(comments.router)
 app.include_router(posts.router)
 
-@app.get("/")
+@app.get("/health")
 async def health_check():
     return {"Healthy": 200}
 
-@app.get("/docs")
-async def custom_docs():
-    return get_swagger_ui_html(openapi_url="/openapi.json", title="API Docs")
+# @app.get("/docs")
+# async def custom_docs():
+#     return get_swagger_ui_html(openapi_url="/openapi.json", title="API Docs")
